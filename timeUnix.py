a1d0e17ca34f64e0f9535c53e659a3a004de0402
kamikadze3113 <68239296+kamikadze3113@users.noreply.github.com>
2020-08-10 13:27:11 +0300

    Update timeUnix.py

--- timeUnix.py
+++ timeUnix.py
@@ -9,6 +9,6 @@ class TimeMod(loader.Module):
 	@loader.sudo
 	async def tucmd(self, message):
 	    """by kamikazde3113 (Telegram)"""
-	    await message.edit(str(time.time()))
+	    await message.edit("С начала эпохи прошло\n\n" + "<b>" + str(time.time()) + "</b>")
 	async def yearcmd(self, message):
-	    await message.edit("Сейсчас 2020 но он более известный как \n" + "<b>Короновирусный год</b>")
+	    await message.edit("Сейсчас 2020 но он более известный как \n\n" + "<b>Короновирусный год</b>")
